ad05b6eeb31a1fb27451398c4b7b7907d20b44ef
Tyler Werman <wermant@udel.edu>
2022-04-18 14:58:58 -0400

    finished creation functions for all structs

--- structures.c
+++ structures.c
@@ -32,14 +32,53 @@ typedef struct Job{
     int priority;
 }Job;
 
+Job * createJob(char str[]){
+    Job *job = malloc(sizeof(Job));
+    char *token=strtok(str," ");
+    job->arrival=atoi(token);
+    token=strtok(NULL," ");
+    job->job_num=atoi(token+2);
+    token=strtok(NULL," ");
+    job->needed_memory=atoi(token+2);
+    token=strtok(NULL," ");
+    job->num_devices = atoi(token+2);
+    token=strtok(NULL," ");
+    job->run_time = atoi(token+2);
+    token=strtok(NULL," ");
+    job->priority = atoi(token+2);
+    return job;
+}
+
 typedef struct Request{
     int arrival;
     int job_num;
     int num_devices;
 }Request;
 
+Request * createRequest(char str[]){
+    Request *req = malloc(sizeof(Request));
+    char *token=strtok(str," ");
+    req->arrival=atoi(token);
+    token=strtok(NULL," ");
+    req->job_num=atoi(token+2);
+    token=strtok(NULL," ");
+    req->num_devices=atoi(token+2);
+    return req;
+}
+
 typedef struct Release{
     int release_time;
     int job_num;
     int num_devices;
 }Release;
+
+Release * createRelease(char str[]){
+    Release *release = malloc(sizeof(Release));
+    char *token=strtok(str," ");
+    release->release_time=atoi(token);
+    token=strtok(NULL," ");
+    release->job_num=atoi(token+2);
+    token=strtok(NULL," ");
+    release->num_devices=atoi(token+2);
+    return req;
+}
