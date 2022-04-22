66cfa441758e3a21ace69bd9550ee0cfda6d6bc9
Tyler Werman <wermant@udel.edu>
2022-04-22 15:05:33 -0400

    adding pop and push for holdqueue1

--- hold_queue1.c
+++ hold_queue1.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+#include "hold_queue1.h"
+void push(Node *head,Job *new_job){
+    if (head==NULL){
+        head=malloc(sizeof(Node));
+        head->job=new_job;
+        head->next=NULL;
+        head->prev=NULL;
+    }
+    else{
+        Node *new_node=malloc(sizeof(Node));
+        new_node->job=new_job;
+        Node *temp=head;
+        while ((temp->job->run_time <= new_node->job->run_time)&&(temp->next->job->run_time>=new_node->job->run_time)){
+            temp=temp->next;
+        }
+        temp->next->prev=new_node;
+        new_node->next=temp->next;
+        new_node->prev=temp;
+        temp->next=new_node;
+    }
+}
+
+Node *pop(Node *head){
+    Node *ret = head;
+    head=head->next;
+    head->prev=NULL;
+    return ret;
+}
 
--- hold_queue1.h
+++ hold_queue1.h
@@ -1,1 +1,10 @@
+#include "structures.h"
+typedef struct Node{
+    Job *job;
+    Node *next;
+    Node *prev;
+}Node;
+
+void push(Node *,Job *);
+Node *pop(Node *);
 
